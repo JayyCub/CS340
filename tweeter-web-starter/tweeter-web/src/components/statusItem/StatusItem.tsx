ff4f63b7d74ee63a80c0be4afccfbd88d4b2c7d4
Jacob Thomsen <jat0220@byu.edu>
2024-02-02 16:49:43 -0700

    Finish Milestone 1.3.1 - useUserNavigation.tsx hook implemented

--- tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -1,52 +1,14 @@
 import {Link} from "react-router-dom";
 import Post from "./Post";
-import {AuthToken, FakeData, Status, User} from "tweeter-shared";
-import useToastListener from "../toaster/ToastListenerHook";
-import {useContext} from "react";
-import {UserInfoContext} from "../userInfo/UserInfoProvider";
+import { Status } from "tweeter-shared";
+import useUserNavigation from "./useUserNavigation";
 
 interface Props {
     value: Status;
 }
 
 const StatusItem = (props: Props) => {
-    const { displayErrorMessage } = useToastListener();
-    const { setDisplayedUser, currentUser, authToken } =
-        useContext(UserInfoContext);
-
-    const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-        event.preventDefault();
-
-        try {
-            let alias = extractAlias(event.target.toString());
-
-            let user = await getUser(authToken!, alias);
-
-            if (!!user) {
-                if (currentUser!.equals(user)) {
-                    setDisplayedUser(currentUser!);
-                } else {
-                    setDisplayedUser(user);
-                }
-            }
-        } catch (error) {
-            displayErrorMessage(`Failed to get user because of exception: ${error}`);
-        }
-    };
-
-    const extractAlias = (value: string): string => {
-        let index = value.indexOf("@");
-        return value.substring(index);
-    };
-
-    const getUser = async (
-        authToken: AuthToken,
-        alias: string
-    ): Promise<User | null> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.findUserByAlias(alias);
-    };
-
+    const { navigateToUser } = useUserNavigation();
 
     return (
     <div className="col bg-light mx-0 px-0">
